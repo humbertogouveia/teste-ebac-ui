22939d8f883dc3565f19a290ffb4d4897a687ad8
Humberto Gouveia <humbertogouveia@outlook.com>
2022-02-14 20:26:18 -0300

    teste:Page Object adicionado

--- cypress/integration/endereco.spec.js
+++ cypress/integration/endereco.spec.js
@@ -1,3 +1,5 @@
+import enderecoPage from '../support/page-objects/endereco.page'
+
 const perfil = require("../fixtures/perfil.json")
 
 describe("Funcionalidade de endereços - faturamento e entrega", () => {
@@ -6,5 +8,9 @@ describe("Funcionalidade de endereços - faturamento e entrega", () => {
     cy.login(perfil.usuario, perfil.senha)
   })
 
-  it("Deve fazer cadastro de faturamento com sucesso", () => {})
+  it.only("Deve editar de faturamento com sucesso", () => {
+    enderecoPage.editarEnderecoFaturamento('Humberto','Gouveia','Apple Inc','Brasil','Rua Green','10','São Paulo','São Paulo','08225000')
+    cy.get('.woocommerce-message').should('contain','Endereço alterado com sucesso.')
+    
+  })
 })
